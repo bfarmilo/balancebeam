095975fb387795bfeb27fd810b708fc2643e0e32
Bill Farmilo <bill@farmilostrategy.com>
2018-06-06 14:56:56 -0400

    Added command-line function for reading encrypted files

--- test/components/decrypt.test.js
+++ test/components/decrypt.test.js
@@ -1,7 +1,20 @@
 const { encode, decode } = require('../../app/actions/decrypt');
-const clean = require('../../app/test_results/temp.json');
 const fse = require('fs-extra');
 
+const encodedFile = 'C:\\Users\\bfarm\\Dropbox (Personal)\\Swap\\Budget\\config.json';
+const decodedFile = './app/test_results/tempdec.json';
 
-//fse.writeJSON('./app/test_results/temp.json', decode(clean.payload)).catch(err => console.error(err));
-fse.writeJSON('./app/test_results/tempenc.json', encode(clean)).catch(err => console.error(err));
+const DECODE = false;
+const ENCODE = true;
+
+const readConfig = (inFile, outFile, encodeMode=true) => {
+    fse.readJSON(inFile)
+    .then(clean => {
+        return fse.writeJSON(outFile, encodeMode ? encode(clean) : decode(clean.payload))
+    })
+    .catch(err => console.error(err));
+
+}
+
+//readConfig(decodedFile, encodedFile, ENCODE);
+readConfig(encodedFile, decodedFile, DECODE);
